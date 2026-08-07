fec41a64b50657c682bdc88fade1174cf272060c
ebaze-tech <ebaze.technologies@gmail.com>
2026-08-07 11:56:33 +0100

    bug: fixed token otp emailing logic bug

--- src/accounts/strategy/jwt.strategy.ts
+++ src/accounts/strategy/jwt.strategy.ts
@@ -18,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     const secret = configService.get<string>('JWT_ACCESS_SECRET');
     if (!secret)
-      throw new Error(
+      throw new BadRequestException(
         'JWT_ACCESS_SECRET is not defined in the environment variables',
       );
 
