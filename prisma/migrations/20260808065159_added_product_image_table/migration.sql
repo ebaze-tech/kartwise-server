9ba3b56a2f29360dd92ac7a7cb233b1f660ffc1a
ebaze-tech <ebaze.technologies@gmail.com>
2026-08-11 21:05:14 +0100

    cleared the db and re-initialized the DB

--- prisma/migrations/20260808065159_added_product_image_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "business" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
--- prisma/migrations/20260808124231_added_product_image_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "business" ALTER COLUMN "isActive" SET DEFAULT false;
